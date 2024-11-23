d2a7115f2f903ec58366951649b6b739eb1e7cb6
Beckversync <118ngobach@gmail.com>
2024-11-23 17:25:43 +0700

    no message

--- LAB4_MCU/LAB3/Debug/Core/Src/subdir.mk
+++ LAB4_MCU/LAB3/Debug/Core/Src/subdir.mk
@@ -12,6 +12,7 @@ C_SRCS += \
 ../Core/Src/global.c \
 ../Core/Src/input_reading.c \
 ../Core/Src/main.c \
+../Core/Src/scheduler.c \
 ../Core/Src/stm32f1xx_hal_msp.c \
 ../Core/Src/stm32f1xx_it.c \
 ../Core/Src/syscalls.c \
@@ -26,6 +27,7 @@ OBJS += \
 ./Core/Src/global.o \
 ./Core/Src/input_reading.o \
 ./Core/Src/main.o \
+./Core/Src/scheduler.o \
 ./Core/Src/stm32f1xx_hal_msp.o \
 ./Core/Src/stm32f1xx_it.o \
 ./Core/Src/syscalls.o \
@@ -40,6 +42,7 @@ C_DEPS += \
 ./Core/Src/global.d \
 ./Core/Src/input_reading.d \
 ./Core/Src/main.d \
+./Core/Src/scheduler.d \
 ./Core/Src/stm32f1xx_hal_msp.d \
 ./Core/Src/stm32f1xx_it.d \
 ./Core/Src/syscalls.d \
